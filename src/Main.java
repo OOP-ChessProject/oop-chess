8699b5ce20a41c6f3a8a8dd1943739910ff2c519
tamrattemesgen17 <tamrattemesgen17@gmail.com>
2026-05-03 00:30:20 +0300

    the Main file to start our project

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("OOP Chess - Dev Branch");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 800);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
