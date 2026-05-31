320dcaf6ee1243d09a3f40829337dcf5944bef79
elfazdanielcode <fnfrzf97k9@privaterelay.appleid.com>
2026-05-31 10:38:10 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,1 @@
-import javax.swing.JFrame;
 
-public class Main {
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("OOP Chess - Dev Branch");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800, 800);
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
-    }
-}//the board to start coding for nowK
-// ok
